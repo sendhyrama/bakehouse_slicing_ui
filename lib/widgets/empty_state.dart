890ebc6eab7e5d3f1493f4e1ad8d003de3009692
sendhyrama <sendhyrama@gmail.com>
2024-08-20 06:54:19 +0700

    refactor (merchant-ui): emptyState widget

--- lib/widgets/empty_state.dart
+++ lib/widgets/empty_state.dart
@@ -1,53 +1,45 @@
-import 'package:bakehouse_slicing_ui/common/colors.dart';
-import 'package:bakehouse_slicing_ui/common/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:bakehouse_slicing_ui/common/colors.dart';
+import 'package:bakehouse_slicing_ui/common/text_styles.dart';
 
 class EmptyState extends StatelessWidget {
   final String title;
   final SvgPicture icon;
   final String? message;
 
-  EmptyState({required this.title, required this.icon, this.message});
+  const EmptyState({
+    required this.title,
+    required this.icon,
+    this.message,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          return SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: constraints.maxHeight,
-              ),
-              child: IntrinsicHeight(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      icon,
-                      const SizedBox(height: 16.0),
-                      Text(
-                        title,
-                        style:
-                            TextStyles.h3.copyWith(fontWeight: FontWeight.w600),
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 4.0),
-                      if (message != null)
-                        Text(
-                          message!,
-                          style: TextStyles.b1.copyWith(color: NeutralColor.c5),
-                          textAlign: TextAlign.center,
-                        ),
-                    ],
-                  ),
-                ),
-              ),
+    return Center(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            icon,
+            const SizedBox(height: 16.0),
+            Text(
+              title,
+              style: TextStyles.h3.copyWith(fontWeight: FontWeight.w600),
+              textAlign: TextAlign.center,
             ),
-          );
-        },
+            if (message != null) ...[
+              const SizedBox(height: 4.0),
+              Text(
+                message!,
+                style: TextStyles.b1.copyWith(color: NeutralColor.c5),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ],
+        ),
       ),
     );
   }
